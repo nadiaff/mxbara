3159aec42a9a4c99c834e202316c9f06fc04cd1a
Nadia <nadiaff@gmail.com>
2015-05-17 04:05:13 -0400

    Delete logo.php

--- logo.php
+++
@@ -1,8 +0,0 @@
-    <div class="row">
-		<div class="large-3 hide-for-medium columns">
-		</div>
-        <div class="large-9 medium-12 small-12 columns">
-			<a href="http://www.drweeaboo.net/comics/MXBARA/"><img src="/comics/MXBARA/img/mxbaralogo.png"/></a>
-        </div>
-    </div>
-  
